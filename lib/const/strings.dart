7087d42d6ae531332e1c649f67dd764c797276bd
soft_code <emmanuel.erhabor98@gmail.com>
2023-03-07 23:45:00 +0100

    pivots

--- lib/const/strings.dart
+++ lib/const/strings.dart
@@ -1,8 +1,11 @@
 class Strings{
   static const String pivotP = 'Pivot Points Calculator';
-  static const String s1 = 'S1';
-  static const String s2 = 'S2';
-  static const String r1 = 'R1';
-  static const String r2 = 'R2';
+  static const String s1 = 'S1:';
+  static const String s2 = 'S2:';
+  static const String r1 = 'R1:';
+  static const String r2 = 'R2:';
   static const String pivot = 'Pivot';
+  static const String high = 'High';
+  static const String low = 'Low';
+  static const String close = 'Close';
 }
--- lib/pivotPoint.dart
+++ lib/pivotPoint.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import 'commonWidgets/formNum.dart';
 import 'const/strings.dart';
 import 'const/styles.dart';
@@ -13,6 +12,10 @@ class PivotPoints extends StatefulWidget {
 }
 
 class _PivotPointsState extends State<PivotPoints> {
+  final TextEditingController _highController = TextEditingController();
+  final TextEditingController _lowController = TextEditingController();
+  final TextEditingController _closeController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,28 +25,28 @@ class _PivotPointsState extends State<PivotPoints> {
           padding: EdgeInsets.only(left: 10.r, right: 10.r, top: 30.r),
           child: Column(
             children: [
-              Center(child: Text(Strings.pivotP)),
+              Center(child: Text(Strings.pivotP, style: kFirstN,)),
               SizedBox(height: 20.h,),
-              const FormNum(text: Strings.s1, hText: Strings.s1,),
+              FormNum(text: Strings.high, hText: Strings.high, controller: _highController),
               SizedBox(height: 10.h,),
-              const FormNum(text: Strings.s1, hText: Strings.s1,),
+              FormNum(text: Strings.low, hText: Strings.low, controller: _lowController,),
               SizedBox(height: 10.h,),
-              const FormNum(text: Strings.r1, hText: Strings.r1,),
-              SizedBox(height: 10.h,),
-              const FormNum(text: Strings.r2, hText: Strings.r2,),
+              FormNum(text: Strings.close, hText: Strings.close, controller: _closeController,),
               SizedBox(height: 20.h,),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(Strings.s1,style: kFirstN,),
+                  SizedBox(width: 10.w,),
                   Text(Strings.s2,style: kFirstN,)
                 ],
               ),
-              SizedBox(height: 20.h,),
+              SizedBox(height: 10.h,),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(Strings.r1,style: kFirstN,),
+                  SizedBox(width: 10.w,),
                   Text(Strings.r2,style: kFirstN,)
                 ],
               ),
